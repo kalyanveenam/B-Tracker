cbce4df58efe1625e4b5caf9cc3e00d246c9273f
unknown <kalyanveenam@gmail.com>
2020-08-07 10:21:03 +0530

    Design for signin component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import {SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [HomeComponent, SigninComponent, SignupComponent
       ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    RouterModule
   ]
 })
 export class UserModule { }
